ec125230987c0d4b53b8666f6b32b702202a6f0b
HIPPALGAONKAR <r.hippalgaonkar@dxc.com>
2022-01-21 11:17:11 +0530

    first commit

--- src/main/java/com/dxc/assessment/App.java
+++ src/main/java/com/dxc/assessment/App.java
@@ -1,13 +1,37 @@
 package com.dxc.assessment;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    private static final String SQL_CREATE= "CREATE TABLE AUTHORS"
+      +"(" 
+      + "id int not null auto_increment,"
+      +"first_name varchar(255),"
+       +"last_name varchar(255),"
+      +  "genre varchar(50),"
+      +"email varchar(50),"
+      + "primary key (id)"
+    
+       +" )";
+
+        
+    
+    public static void main( String[] args ) throws SQLException
     {
         System.out.println( "Hello World!" );
-    }
-}
+
+        Connection conn =DriverManager.getConnection("com.mysql.jdbc.Driver");
+        Statement Statement= conn.createStatement();
+            Statement.execute(SQL_CREATE);
+            
+        }
+   }
+
